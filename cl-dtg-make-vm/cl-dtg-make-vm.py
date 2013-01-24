6f6e79d85d4ce4bf41b6336f96bc122cdea5c24e
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2013-01-24 00:03:53 +0000

    Modified the cl-dtg-make-vm script so that we now build two disks for each VM. The intended use for the disks is that one stores the OS, and the other stores /local/data. This lets us swap the OS, whilst keeping the data.
    I had to do some hackery, because of nastyness in xe. xe vm-install creates
    a vdi of 8GB, and attaches it to the guest. The size cannot be specified.
    You cannot shrink the size of a vdi. Therefore, I have to delete it
    straight after creation.

--- cl-dtg-make-vm/cl-dtg-make-vm.py
+++ cl-dtg-make-vm/cl-dtg-make-vm.py
@@ -14,9 +14,10 @@ INSTALLREPO       = 'http://www-uxsup.csx.cam.ac.uk/pub/linux/ubuntu'
 PRESEEDLOCATION   = 'http://puppy8.dtg.cl.cam.ac.uk/alloptions.cfg'
 
 # Filesystem
-DEFAULTROOTFSSIZE = '15'
+DEFAULTROOTFSSIZE = '4'
+DEFAULTDATAFSSIZE = '1'
 DEFAULTSR         = 'NFS\ virtual\ disk\ storage'
-
+SR                = 'b1bb7fa6-ca99-c984-4002-940ac6757e3e'
 # Networking
 NETWORK           = '77922c07-8ea4-abb2-708b-4719f4c6a0f8'
 DEVICE            = '0'
@@ -27,7 +28,7 @@ DEFAULTMEMORY     = '2048'
 
 
 @hosts(dom0)
-def new_vm(name, ip="", mac="", memory=DEFAULTMEMORY, vcpus=DEFAULTVCPUs, root_fs_size=DEFAULTROOTFSSIZE, fs_location=DEFAULTSR):
+def new_vm(name, ip="", mac="", memory=DEFAULTMEMORY, vcpus=DEFAULTVCPUs, root_fs_size=DEFAULTROOTFSSIZE, data_size=DEFAULTDATAFSSIZE, fs_location=SR):
     """
     Create a new VM
     """
@@ -37,7 +38,7 @@ def new_vm(name, ip="", mac="", memory=DEFAULTMEMORY, vcpus=DEFAULTVCPUs, root_f
         mac = next_mac()
 
     # Create a VM
-    new_vm = run('xe vm-install new-name-label=%s template=%s sr-name-label=%s' % (name, TEMPLATE, fs_location))
+    new_vm = run('xe vm-install new-name-label=%s template=%s sr-uuid=%s' % (name, TEMPLATE, fs_location))
 
     # Point VM at install repo
     run('xe vm-param-set uuid=%s other-config:install-repository=%s' % (new_vm, INSTALLREPO))
@@ -45,9 +46,18 @@ def new_vm(name, ip="", mac="", memory=DEFAULTMEMORY, vcpus=DEFAULTVCPUs, root_f
     # Give the VM a VIF
     run('xe vif-create network-uuid=%s mac=%s vm-uuid=%s device=%s' % (NETWORK, mac, new_vm, DEVICE))
 
-    # Set the disk size
-    vdi_uuid = run('xe vm-disk-list vm=%s |grep uuid | sed -e \'s/.*: //\' | tail -n 1' % name)
-    run('xe vdi-resize uuid=%s disk-size=%sGiB' % ( vdi_uuid, root_fs_size))
+    # Remove the first disk. XAPI adds an 8GiB disk to the VM with vm-install
+    # and this can't be reduced in size.
+    vdi_old_uuid = run('xe vm-disk-list vm=%s |grep uuid | sed -e \'s/.*: //\' | tail -n 1' % name)
+    vbd_uuid = run('xe vbd-list params=uuid vm-uuid=%s --minimal' % new_vm)
+    run('xe vbd-destroy uuid=%s' % vbd_uuid)
+    run('xe vdi-destroy uuid=%s' % vdi_old_uuid)
+
+    # Create new disks
+    vdi_a_uuid = run('xe vdi-create sr-uuid=%s virtual-size=%sGiB name-label=%s-os type=user' % (fs_location, root_fs_size, name))
+    run('xe vbd-create vm-uuid=%s vdi-uuid=%s device=0 bootable=true' % (new_vm, vdi_a_uuid))
+    vdi_b_uuid = run('xe vdi-create sr-uuid=%s virtual-size=%sGiB name-label=%s-data type=user' % (fs_location, data_size, name))
+    run('xe vbd-create vm-uuid=%s vdi-uuid=%s device=1' % (new_vm, vdi_b_uuid))
 
     # Give the VM enough memory, and VCPU
     run('xe vm-param-set uuid=%s VCPUs-max=%s' % (new_vm, vcpus))
