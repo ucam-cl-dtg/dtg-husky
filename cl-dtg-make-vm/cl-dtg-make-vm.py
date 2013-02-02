9841e63a68d29e25f6da213296fe51ae92aa8bd2
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2013-02-02 21:43:31 +0000

    Added support for building VMs from a template

--- cl-dtg-make-vm/cl-dtg-make-vm.py
+++ cl-dtg-make-vm/cl-dtg-make-vm.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python
 
 from fabric.api import *
+from datetime import date
+from time import sleep
 import sys, subprocess
 
 dhcp = 'husky0.dtg.cl.cam.ac.uk'
@@ -18,6 +20,7 @@ DEFAULTROOTFSSIZE = '4'
 DEFAULTDATAFSSIZE = '1'
 DEFAULTSR         = 'NFS\ virtual\ disk\ storage'
 SR                = 'b1bb7fa6-ca99-c984-4002-940ac6757e3e'
+
 # Networking
 NETWORK           = '77922c07-8ea4-abb2-708b-4719f4c6a0f8'
 DEVICE            = '0'
@@ -26,25 +29,41 @@ DEVICE            = '0'
 DEFAULTVCPUs      = '2'
 DEFAULTMEMORY     = '2048'
 
+# Template
+TEMPLATENAME      = 'DTG-template'
 
-@hosts(dom0)
-def new_vm(name, ip="", mac="", memory=DEFAULTMEMORY, vcpus=DEFAULTVCPUs, root_fs_size=DEFAULTROOTFSSIZE, data_size=DEFAULTDATAFSSIZE, fs_location=SR):
-    """
-    Create a new VM
-    """
+# SSH
+SSHUSER           = 'dtg'
+
+def prepare_vm(ip, mac, uuid, memory, vcpus):
+
     if ip != "":
         mac = ip_to_mac(ip)
     if mac == "":
         mac = next_mac()
+
+    # Give the VM a VIF
+    run('xe vif-create network-uuid=%s mac=%s vm-uuid=%s device=%s' % (NETWORK, mac, uuid, DEVICE))
+
+    # Give the VM enough memory, and VCPU
+    run('xe vm-param-set uuid=%s VCPUs-max=%s' % (uuid, vcpus))
+    run('xe vm-param-set uuid=%s  VCPUs-at-startup=%s'  % (uuid,vcpus))
+    run('xe vm-memory-limits-set uuid=%s dynamic-max=%sMiB static-max=%sMiB static-min=%sMiB dynamic-min=%sMiB' % (uuid, memory, memory, memory, memory))
+
+
+@hosts(dom0)
+def new_vm(name="", ip="", mac="", memory=DEFAULTMEMORY, vcpus=DEFAULTVCPUs, root_fs_size=DEFAULTROOTFSSIZE, fs_location=SR):
+    """
+    Create a new VM
+    """
+    if name == "":
+        name='DTG-snapshot-' + date.today()
 
     # Create a VM
     new_vm = run('xe vm-install new-name-label=%s template=%s sr-uuid=%s' % (name, TEMPLATE, fs_location))
 
     # Point VM at install repo
     run('xe vm-param-set uuid=%s other-config:install-repository=%s' % (new_vm, INSTALLREPO))
-
-    # Give the VM a VIF
-    run('xe vif-create network-uuid=%s mac=%s vm-uuid=%s device=%s' % (NETWORK, mac, new_vm, DEVICE))
 
     # Remove the first disk. XAPI adds an 8GiB disk to the VM with vm-install
     # and this can't be reduced in size.
@@ -56,19 +75,47 @@ def new_vm(name, ip="", mac="", memory=DEFAULTMEMORY, vcpus=DEFAULTVCPUs, root_f
     # Create new disks
     vdi_a_uuid = run('xe vdi-create sr-uuid=%s virtual-size=%sGiB name-label=%s-os type=user' % (fs_location, root_fs_size, name))
     run('xe vbd-create vm-uuid=%s vdi-uuid=%s device=0 bootable=true' % (new_vm, vdi_a_uuid))
-    vdi_b_uuid = run('xe vdi-create sr-uuid=%s virtual-size=%sGiB name-label=%s-data type=user' % (fs_location, data_size, name))
-    run('xe vbd-create vm-uuid=%s vdi-uuid=%s device=1' % (new_vm, vdi_b_uuid))
-
-    # Give the VM enough memory, and VCPU
-    run('xe vm-param-set uuid=%s VCPUs-max=%s' % (new_vm, vcpus))
-    run('xe vm-param-set uuid=%s  VCPUs-at-startup=%s'  % (new_vm,vcpus))
-    run('xe vm-memory-limits-set uuid=%s dynamic-max=%sMiB static-max=%sMiB static-min=%sMiB dynamic-min=%sMiB' % (new_vm, memory, memory, memory, memory))
 
     # Set the preseed file, and pass a hostname, and password hash
     run('xe vm-param-set uuid=%s PV-args=" --quiet console=hvc0 auto=true url=%s netcfg/get_hostname=%s"' % (new_vm, PRESEEDLOCATION, name))
 
+    prepare_vm(ip, mac, new_vm, memory, vcpus)
+
     # Boot the VM, with an answer file
     run('xe vm-start vm=%s' % name)
+
+    # Start a background script in dom0 that waits for installation to complete, then shuts down the VM, and builds a snapshot
+    run('nohup ./capture-vm-snapshot.sh %s &' % name)
+
+
+@hosts(dom0)
+def new_cloned_vm(name, ip="", mac="", memory=DEFAULTMEMORY, vcpus=DEFAULTVCPUs, data_size=DEFAULTDATAFSSIZE, data_SR=SR):
+    """
+    Build a new VM by cloning the most recent DTG-snapshot.
+    This will give a DTG-itised VM, much faster than calling new_vm,
+    however the VM uses copy-on-write.
+    """
+
+    # Create VM from snapshot
+    run('xe vm-install new-name-label=%s template=%s sr-uuid=%s' % (name, TEMPLATENAME, SR))
+    new_vm = run('xe vm-list name-label=%s params=uuid --minimal' % name)
+
+    prepare_vm(ip, mac, new_vm, memory, vcpus)
+
+    # Create a /dev/xvdb that can be mounted at /data/local
+    if data_size > 0:
+        vdi_b_uuid = run('xe vdi-create sr-uuid=%s virtual-size=%sGiB name-label=%s-data type=user' % (data_SR, data_size, name))
+        run('xe vbd-create vm-uuid=%s vdi-uuid=%s device=1' % (new_vm, vdi_b_uuid))
+
+    run('xe vm-start vm=%s' % name)
+
+    # SSH into the new machine, and set the hostname. First wait for the m/c to boot
+
+    sleep(10)
+    ip = run('xe vm-param-get param-name=networks uuid=%s | sed -e \'s_0/ip: __\' -e \'s/; .*$//\'' % new_vm)
+    subprocess.call(['ssh %s@%s "hostname %s"'  % (SSHUSER, ip, name)])
+    subprocess.call(['ssh %s@%s "./etc/puppet-bare/hooks/post-update"' % (SSHUSER, ip)])
+
 
 @hosts(dhcp)
 def ip_to_mac(ip):
